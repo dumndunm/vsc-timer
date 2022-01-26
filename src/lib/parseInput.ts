e6760e566437b786b1cbd3758cd72e76625b4e94
I. Leonov <dumndunm@gmail.com>
2022-01-26 23:49:24 +0300

    Fix time units logic

--- src/lib/parseInput.ts
+++ src/lib/parseInput.ts
@@ -4,13 +4,17 @@ const getTimeQuantity = (input: string): number => {
   return Number(input.slice(0, -1));
 };
 
+const isTimeUnits = (timeUnits: string): timeUnits is TimeUnits => {
+  return Object.values<string>(TimeUnits).includes(timeUnits);
+};
+
 const getTimeUnits = (input: string): TimeUnits | null => {
   const timeUnits = input.slice(-1);
-  return timeUnits in TimeUnits ? (timeUnits as TimeUnits) : null;
+  return isTimeUnits(timeUnits) ? timeUnits : null;
 };
 
 const parseInput = (input: string): [number, TimeUnits] => {
   return [getTimeQuantity(input), getTimeUnits(input) ?? DEFAULT_TIME_UNITS];
 };
 
-export { TimeUnits, DEFAULT_TIME_UNITS, parseInput };
+export { parseInput };
